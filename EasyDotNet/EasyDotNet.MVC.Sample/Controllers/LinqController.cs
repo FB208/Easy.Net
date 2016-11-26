918b38bbcf0a7f5d89e20dd7c4037127eaeda60c
杨惠超 <472189065@qq.com>
2016-11-26 13:00:57 +0800

    DistinctExtensions

---
+++ EasyDotNet/EasyDotNet.MVC.Sample/Controllers/LinqController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EasyDotNet.LinQ;
+
+namespace EasyDotNet.MVC.Sample.Controllers
+{
+    public class LinqController : Controller
+    {
+        class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+        // GET: Linq
+        public string DistinctExtensions()
+        {
+            List<User> users = new List<User>()
+            {
+                new User() {Name="张三",Age=10 },
+                new User() {Name="李四",Age=12 },
+                new User() {Name="王五",Age=10 },
+                new User() {Name="赵六",Age=20 }
+            };
+
+            users=users.Distinct(m => m.Age).ToList();
+
+            string resultStr = "";
+            foreach (var user in users)
+            {
+                resultStr += user.Name+"<br/>";
+            }
+            return resultStr;
+        }
+    }
+}
---
+++ EasyDotNet/EasyDotNet/LinQ/DistinctExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyDotNet.LinQ
+{
+    /// <summary>
+    /// 扩展LinQ.Distinct方法
+    /// var list = list.Distinct(m=>m.variableName);//去掉variableName重复的数据
+    /// </summary>
+    public static class DistinctExtensions
+    {
+        public static IEnumerable<T> Distinct<T, V>(this IEnumerable<T> source, Func<T, V> keySelector)
+        {
+            return source.Distinct(new CommonEqualityComparer<T, V>(keySelector));
+        }
+    }
+    public class CommonEqualityComparer<T, V> : IEqualityComparer<T>
+    {
+        private Func<T, V> keySelector;
+
+        public CommonEqualityComparer(Func<T, V> keySelector)
+        {
+            this.keySelector = keySelector;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return EqualityComparer<V>.Default.Equals(keySelector(x), keySelector(y));
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return EqualityComparer<V>.Default.GetHashCode(keySelector(obj));
+        }
+    }
+    
+}
