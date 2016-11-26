6d35fa179770ccb6c7e6d6a27ed9d18e71d2b96d
杨惠超 <472189065@qq.com>
2016-11-26 12:30:54 +0800

    初始化项目

---
+++ EasyDotNet.MVC.Sample/EasyDotNet.MVC.Sample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EasyDotNet.MVC.Sample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
