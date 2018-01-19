56f36322c1d3d0816b88258082bd3a06c557da55
杨惠超 <472189065@qq.com>
2018-01-19 14:46:47 +0800

    提取公共测试模型

--- EasyDotNet/EasyDotNet.MVC.Sample/Controllers/LinqController.cs
+++ EasyDotNet/EasyDotNet.MVC.Sample/Controllers/LinqController.cs
@@ -4,26 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EasyDotNet.LinQ;
+using EasyDotNet.MVC.Sample.Models;
 
 namespace EasyDotNet.MVC.Sample.Controllers
 {
     public class LinqController : Controller
     {
-        class User
-        {
-            public string Name { get; set; }
-            public int Age { get; set; }
-        }
+        
         // GET: Linq
         public string DistinctExtensions()
         {
-            List<User> users = new List<User>()
-            {
-                new User() {Name="张三",Age=10 },
-                new User() {Name="李四",Age=12 },
-                new User() {Name="王五",Age=10 },
-                new User() {Name="赵六",Age=20 }
-            };
+            List<User> users = new User().GetList();
 
             users=users.Distinct(m => m.Age).ToList();
 
---
+++ EasyDotNet/EasyDotNet.MVC.Sample/Models/Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyDotNet.MVC.Sample.Models
+{
+    
+    public class User
+    {
+        private User user;
+        public User(){
+            user = new User();
+            }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User GetModel()
+        {
+            user = new User()
+            {
+                Id=1,Name="张三",Age=20
+            };
+            return user;
+        }
+
+        public List<User> GetList()
+        {
+            List<User> list = new List<User>()
+            {
+                new User() {Id=1,Name="张三",Age=10 },
+                new User() {Id=2,Name="李四",Age=12 },
+                new User() {Id=3,Name="王五",Age=10 },
+                new User() {Id=4,Name="赵六",Age=20 }
+            };
+            return list;
+        }
+    }
+}
