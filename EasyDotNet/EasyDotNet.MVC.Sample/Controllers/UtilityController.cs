17e4dfa4ad3852a188e04baa79c60f3498f3055f
杨惠超 <472189065@qq.com>
2018-01-19 16:11:57 +0800

    XmlHelper使用示例

--- EasyDotNet/EasyDotNet.MVC.Sample/Controllers/UtilityController.cs
+++ EasyDotNet/EasyDotNet.MVC.Sample/Controllers/UtilityController.cs
@@ -1,17 +1,86 @@
-﻿using System;
+﻿using EasyDotNet.MVC.Sample.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace EasyDotNet.MVC.Sample.Controllers
 {
     public class UtilityController : Controller
     {
-        // GET: Utility
-        public ActionResult XmlHelper()
+        /// <summary>
+        /// Class序列化
+        /// </summary>
+        /// <returns></returns>
+        public string Serializer_Class()
         {
-            return View();
+            List<User> users = new User().GetList();
+            var result=EasyDotNet.Utility.XmlHelper.Serializer(typeof(List<User>), users);
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            return xml.InnerXml.ToString();
         }
+
+        /// <summary>
+        /// Class反序列化
+        /// </summary>
+        /// <returns></returns>
+        public object Deserialize_Class(string xmlStr)
+        {
+            if (string.IsNullOrWhiteSpace(xmlStr))
+            {
+                xmlStr =
+                    "<ArrayOfUser><User><Id>1</Id><Name>张三</Name><Age>10</Age></User><User><Id>2</Id><Name>李四</Name><Age>12</Age></User><User><Id>3</Id><Name>王五</Name><Age>10</Age></User><User><Id>4</Id><Name>赵六</Name><Age>20</Age></User></ArrayOfUser>";
+            }
+            var result = EasyDotNet.Utility.XmlHelper.Deserialize(typeof(List<User>), xmlStr);
+            return result;
+        }
+        /// <summary>
+        /// DataTable序列化
+        /// </summary>
+        /// <returns></returns>
+        public string Serializer_DataTable()
+        {
+            // 生成DataTable对象用于测试
+            DataTable dt1 = new DataTable("mytable");   // 必须指明DataTable名称
+
+            dt1.Columns.Add("Dosage", typeof(int));
+            dt1.Columns.Add("Drug", typeof(string));
+            dt1.Columns.Add("Patient", typeof(string));
+            dt1.Columns.Add("Date", typeof(DateTime));
+
+            // 添加行
+            dt1.Rows.Add(25, "Indocin", "David", DateTime.Now);
+            dt1.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+            dt1.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+            dt1.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+            dt1.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+
+            // 序列化
+            string xmlStr = EasyDotNet.Utility.XmlHelper.Serializer(typeof(DataTable), dt1);
+            return xmlStr;
+        }
+        /// <summary>
+        /// DataTable反序列化
+        /// </summary>
+        /// <returns></returns>
+        public object Deserialize_DataTable(string xmlStr)
+        {
+            if (string.IsNullOrWhiteSpace(xmlStr))
+            {
+                
+                //先序列化一个
+                xmlStr =
+                    Serializer_DataTable();
+            }
+            // 反序列化成DataTable对xml的格式要求比较严格，如果不是由DataTable序列化来的xml而是手写的xml很难反序列化成功
+            DataTable dt2 = EasyDotNet.Utility.XmlHelper.Deserialize(typeof(DataTable), xmlStr) as DataTable;
+
+            return dt2;
+        }
+
     }
 }
--- EasyDotNet/EasyDotNet.MVC.Sample/Models/Users.cs
+++ EasyDotNet/EasyDotNet.MVC.Sample/Models/Users.cs
@@ -10,7 +10,7 @@ namespace EasyDotNet.MVC.Sample.Models
     {
         private User user;
         public User(){
-            user = new User();
+           
             }
         public int Id { get; set; }
         public string Name { get; set; }
